c3afff29904b1a01fc3f814bc71967a3dc072526
xiezhm <475958179@qq.com>
2018-04-24 17:37:32 +0800

    修改：      day02.go

--- day02.go
+++ day02.go
@@ -1,8 +1,25 @@
 package main
 
+import (
+	"fmt"
+)
+
+//
+//func main()  {
+//	for i:=0;i<=1000;i++{
+//		println(i,"我是go语言哦!")
+//	}
+//}
+
 func main()  {
-	for i:=0;i<=1000;i++{
-		println(i)
-		println("我是go语言哦!")
+	var a float64
+	for i:=528;i<=535;i++{
+		a = 64986/float64(i)
+		fmt.Print(a,"\n")
+	}
+	fmt.Print("\n")
+	for m:=121;m<=123;m++{
+		a = 64986/float64(m)
+		fmt.Print(a,"\n")
 	}
 }
